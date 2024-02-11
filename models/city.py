b0ff6a4db6b14c56c2e8abea9289d4b97a613207
Abeermohsien <aboorakhalil993@gmail.com>
2024-02-11 13:53:28 -0800

    $

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+models 
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    city id1
+    """
+
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""
+base
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    state
+    """
+
+    name = ""
