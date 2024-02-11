f16822c48a05cb21e8de4a6d32060a4759f9ebfa
Abeermohsien <aboorakhalil993@gmail.com>
2024-02-11 06:43:27 -0800

    $

--- models/base_model.py
+++ models/base_model.py
@@ -40,11 +40,11 @@ class BaseModel:
 
 if __name__ == "__main__":
     my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:") 
