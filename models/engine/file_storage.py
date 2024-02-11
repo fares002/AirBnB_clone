2ec8e736ae1351d2e18fb2af0ff0ffbcc8458765
Abeermohsien <aboorakhalil993@gmail.com>
2024-02-11 10:38:21 -0800

    $

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,26 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
-AirBnB clone File storage
-"""
-
-import json
-=======
 module
 """
 
 import json
 import os
->>>>>>> 281e15e5d5aa3ae94c715583453a3205c97157ff
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
     """
-<<<<<<< HEAD
-    serializes instances to a JSON file and deserializes
-    JSON file to instances
 
     """
 
     __file_path = "file.json"
-=======
-    file storage
-    """
-    __file_path = "file.json"
 
->>>>>>> 281e15e5d5aa3ae94c715583453a3205c97157ff
     __objects = {}
 
     def new(self, obj):
         """
-<<<<<<< HEAD
-        sets in __objects the obj with
-        key <obj class name>.id
+
         """
 
         obj_class_name = obj.__class__.__name__
@@ -47,49 +31,10 @@ class FileStorage:
         """
         returns the dictionary __objects
         """
-
-=======
-        new
-        """
-        obj_cls_name = obj.__class__.__name__
-
-        key = "{}.{}".format(obj_cls_name, obj.id)
-
-        FileStorage.__objects[key] = obj
-
-    def all(self):
-        """
-        alla
-        """
->>>>>>> 281e15e5d5aa3ae94c715583453a3205c97157ff
         return FileStorage.__objects
 
     def save(self):
         """
-<<<<<<< HEAD
-        serializes __objects to the JSON file (path: __file_path)
-        """
-
-        all_objs = FileStorge.__objects
-        obj_dict = {}
-        for key, obj in all_obj.items():
-            obj_dict[key] = obj.to_dict()
-
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dumb(obj_dict, file)
-
-    def reload(self):
-        """
-        deserializes the JSON file to __objects
-        """
-        if os.path.isfile(FileStorge.__file_path):
-            with open(FileStorge.__file_path, "r", encoding="utf-8"):
-                try:
-                    obj_dict = json.load(file)
-                    for key, value in obj_dict.item():
-                        obj = self.class_dict[value['__class__']](**value)
-                        self.__objects[key] = obj
-=======
 
         """
         lobj = FileStorage.__objects
@@ -119,6 +64,5 @@ class FileStorage:
                         instance = cls(**values)
 
                         FileStorage.__objects[key] = instance
->>>>>>> 281e15e5d5aa3ae94c715583453a3205c97157ff
                 except Exception:
                     pass
